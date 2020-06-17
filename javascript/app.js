4ad0941af691f2b730dd70de70ebbbf06d92de9a
ayandebnath <ayandebnath000@gmail.com>
2020-06-17 17:21:47 +0530

    initial commit

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  let width = $(window).width();
+  if (width < 480) {
+    $('.sidenav').sidenav();
+    $('#nav-hide').hide();
+  } else {
+    $('#nav-btn').hide();
+  }
+});
