002cd0c37deb1aa5efc93fa9ec461dc00e22a5dd
Vipin Mishra <52159805+thevipinmishra@users.noreply.github.com>
2020-07-30 19:32:02 +0530

    Delete app.js

--- javascript/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  let width = $(window).width();
-  if (width <= 480) {
-    $('.sidenav').sidenav();
-    $('#nav-hide').hide();
-  } else {
-    $('#nav-btn').hide();
-  }
-});
